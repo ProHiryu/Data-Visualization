6de4679f1628a7ef2c7df0674e517eea189acac2
ProHiryu <songheqi1996@gmail.com>
2017-05-24 17:57:07 +0800

    Univariate_distributions

--- code/seaborn/Distribution/Univariate_distributions.py
+++ code/seaborn/Distribution/Univariate_distributions.py
@@ -6,7 +6,18 @@ import seaborn as sns
 
 sns.set(color_codes=True)
 
-np.random.seed(sum(map(ord, "distributions")))
-
 x = np.random.normal(size=100)
-sns.distplot(x);
+
+# normal histogram with KDE(kernel density estimate)
+sns.distplot(x)
+
+sns.distplot(x, hist=False, rug=True);
+
+sns.kdeplot(x, shade=True);
+
+sns.kdeplot(x)
+sns.kdeplot(x, bw=.2, label="bw: 0.2")
+sns.kdeplot(x, bw=2, label="bw: 2")
+plt.legend();
+
+sns.plt.show()
