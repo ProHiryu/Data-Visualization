5ea126a0e212888e5cd4c1cf146c1392cc90750b
ProHiryu <songheqi1996@gmail.com>
2017-05-26 20:18:27 +0800

    visualize the pair relationship

---
+++ code/seaborn/Distribution/Visualizing_pairwise_relationships.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from scipy import stats, integrate
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+iris = sns.load_dataset("iris")
+sns.pairplot(iris);
+
+sns.plt.show()
