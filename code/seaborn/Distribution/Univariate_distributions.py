839931724a4c99987092d32d6e13a0cfaba802d1
ProHiryu <songheqi1996@gmail.com>
2017-05-26 17:05:48 +0800

    normal

--- code/seaborn/Distribution/Univariate_distributions.py
+++ code/seaborn/Distribution/Univariate_distributions.py
@@ -11,7 +11,15 @@ x = np.random.normal(size=100)
 # normal histogram with KDE(kernel density estimate)
 sns.distplot(x)
 
+# plot without kde
+sns.distplot(x, kde=False, rug=True);
+
+# plot without hist
 sns.distplot(x, hist=False, rug=True);
+
+# the width of each hist,bins grows,the width get down
+sns.distplot(x, bins=20, kde=False, rug=True);
+
 
 sns.kdeplot(x, shade=True);
 
@@ -19,5 +27,7 @@ sns.kdeplot(x)
 sns.kdeplot(x, bw=.2, label="bw: 0.2")
 sns.kdeplot(x, bw=2, label="bw: 2")
 plt.legend();
+
+sns.distplot(x, kde=False, fit=stats.gamma);
 
 sns.plt.show()
