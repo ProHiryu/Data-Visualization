ce5836186def007b160dc26fa92b7590b27ea53e
ProHiryu <songheqi1996@gmail.com>
2017-05-27 23:01:52 +0800

    finish distribution

--- code/seaborn/Distribution/Visualizing_pairwise_relationships.py
+++ code/seaborn/Distribution/Visualizing_pairwise_relationships.py
@@ -9,6 +9,9 @@ iris = sns.load_dataset("iris")
 # the normal plot of pair dataset
 sns.pairplot(iris);
 
-
+# use kde to plot pair dataset(the default camp is green,but the line is blue)
+g = sns.PairGrid(iris)
+g.map_diag(sns.kdeplot)
+g.map_offdiag(sns.kdeplot, cmap="Blues_d", n_levels=6);
 
 sns.plt.show()
