6c0be6f6df3d77318fea06f01d384b9859881668
ProHiryu <songheqi1996@gmail.com>
2017-06-02 15:28:57 +0800

    finish shape

--- code/seaborn/linear_relationships/Controlling_size_shape.py
+++ code/seaborn/linear_relationships/Controlling_size_shape.py
@@ -12,6 +12,17 @@ anscombe = sns.load_dataset("anscombe")
 
 # can reshape the size of graph
 f, ax = plt.subplots(figsize=(5, 6))
-sns.regplot(x="total_bill", y="tip", data=tips, ax=ax);
+sns.regplot(x="total_bill", y="tip", data=tips, ax=ax)
+
+# use size and aspect can also set the shape
+# size is the size of graph,aspect is the width/height
+sns.lmplot(x="total_bill", y="tip", col="day", data=tips,
+           col_wrap=2, size=3)
+sns.lmplot(x="total_bill", y="tip", col="day", data=tips,
+           col_wrap=2, size=4)
+sns.lmplot(x="total_bill", y="tip", col="day", data=tips,
+           aspect=.5)
+sns.lmplot(x="total_bill", y="tip", col="day", data=tips,
+           aspect=.2)
 
 sns.plt.show()
