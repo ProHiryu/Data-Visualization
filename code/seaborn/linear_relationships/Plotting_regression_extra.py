8e6de9efae0dffdb3b31921814df6c6ea2c958fc
ProHiryu <songheqi1996@gmail.com>
2017-06-02 19:55:07 +0800

    finished extra

--- code/seaborn/linear_relationships/Plotting_regression_extra.py
+++ code/seaborn/linear_relationships/Plotting_regression_extra.py
@@ -10,5 +10,16 @@ np.random.seed(sum(map(ord, "regression")))
 tips = sns.load_dataset("tips")
 anscombe = sns.load_dataset("anscombe")
 
-
+# use regression in jointplot
 sns.jointplot(x="total_bill", y="tip", data=tips, kind="reg");
+
+# use regression in pairplot
+sns.pairplot(tips, x_vars=["total_bill", "size"], y_vars=["tip"],
+             size=5, aspect=.8, kind="reg");
+
+# in pairplot the hue will not be two graphs
+sns.pairplot(tips, x_vars=["total_bill", "size"], y_vars=["tip"],
+             hue="smoker", size=5, aspect=.8, kind="reg");
+
+
+sns.plt.show()
