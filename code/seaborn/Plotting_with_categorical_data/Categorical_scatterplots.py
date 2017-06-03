20bfa3fe2e06636883cf57deec3166d90aebdf39
ProHiryu <songheqi1996@gmail.com>
2017-06-03 12:09:23 +0800

    scatterplot

---
+++ code/seaborn/Plotting_with_categorical_data/Categorical_scatterplots.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set(style="whitegrid", color_codes=True)
+
+np.random.seed(sum(map(ord, "categorical")))
+titanic = sns.load_dataset("titanic")
+tips = sns.load_dataset("tips")
+iris = sns.load_dataset("iris")
+
+# simple
+sns.stripplot(x="day", y="total_bill", data=tips);
+
+# use jitter to see the distribution
+sns.stripplot(x="day", y="total_bill", data=tips, jitter=True);
+
+# to avoid overlap
+sns.swarmplot(x="day", y="total_bill", data=tips);
