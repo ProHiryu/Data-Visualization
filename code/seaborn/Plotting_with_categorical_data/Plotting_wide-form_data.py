3b934921168137902698ed2d6697ac9847b178dd
ProHiryu <songheqi1996@gmail.com>
2017-06-08 19:22:31 +0800

    - -

---
+++ code/seaborn/Plotting_with_categorical_data/Plotting_wide-form_data.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set(style="whitegrid", color_codes=True)
+
+np.random.seed(sum(map(ord, "categorical")))
+titanic = sns.load_dataset("titanic")
+tips = sns.load_dataset("tips")
+iris = sns.load_dataset("iris")
+
+
+# change orientation
+sns.boxplot(data=iris, orient="h");
+
+# accept pandas and numpy
+sns.violinplot(x=iris.species, y=iris.sepal_length);
